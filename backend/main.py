1070f7656d8eadb9f961dc308bb8c72d51e7de08
Rosvalde MANFO <florientmanfo3@outlook.fr>
2024-04-05 14:23:02 +0200

    Add websocket on endpoint to receive message

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from pydantic import BaseModel
 from typing import List
 import pika
@@ -10,6 +10,23 @@ RABBIT_MQ_HOST = 'rabbitmq'
 
 class Message(BaseModel):
     content: str
+
+class SocketManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    async def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_message(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = SocketManager()
 
 @app.post("/send/")
 async def send_message(message: Message):
@@ -27,20 +44,21 @@ async def send_message(message: Message):
     connection.close()
     return {"status": "message sent"}
 
-@app.get("/receive/")
-async def receive_message():
+@app.websocket("/receive/{user_name}")
+async def receive_message(user_name: str, websocket: WebSocket):
+    await manager.connect(websocket)
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBIT_MQ_HOST))
     channel = connection.channel()
 
     channel.exchange_declare(exchange=EXCHANGE, exchange_type=EXCHANGE_TYPE)
-    result = channel.queue_declare(queue='')
+    result = channel.queue_declare(queue=user_name)
     queue_name = result.method.queue
 
     channel.queue_bind(exchange=EXCHANGE, queue=queue_name)
 
-    method_frame, header_frame, body = channel.basic_get(queue=queue_name, auto_ack=True)
-    if method_frame:
-        print(f" [x] Received {body}")
-        return {"message": body.decode()}
-    else:
-        return {"message": "No messages in queue"}
+    while True:
+        method_frame, header_frame, body = channel.basic_get(queue=queue_name, auto_ack=True)
+        try:
+            await websocket.send_text(body.decode())
+        except WebSocketDisconnect:
+            manager.disconnect(websocket)
